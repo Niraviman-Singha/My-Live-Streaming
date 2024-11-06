936a648ce346f017ec85bae936db896216315715
Niraviman-Singha <niraviman15-11689@diu.edu.bd>
2024-11-06 19:20:22 +0600

    My Live Streaming App frontend design done

---
+++ app/src/main/java/com/example/mylivestreaming/LiveActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mylivestreaming
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_live)
+    }
+}
